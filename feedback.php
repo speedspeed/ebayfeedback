ce668fef93c9634570b80e67c23fa4970fde7a03
Alex Fedorets <santik.alex@gmail.com>
2014-03-17 12:23:48 +0100

    f

--- feedback.php
+++ feedback.php
@@ -68,8 +68,10 @@ foreach ($feedbacks as $feedback) {
 
         if ($name == 'str3') {
             $textData['colors'] = array(
-                'color1' => "grey",
-                'color2' => "grey",
+                //'color1' => "grey",
+                //'color2' => "grey",
+                'color1' => "CCCCCC",
+                'color2' => "CCCCCC",
             );
         }
 
